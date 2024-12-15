9a61699d8f73fa60e4fd36fafa1c555aad057a0f
maksymPr424 <154377610+maksymPr424@users.noreply.github.com>
2024-12-15 18:49:22 +0100

    15.12

---
+++ 15.12/1.js
@@ -0,0 +1,8 @@
+//TODO: Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+console.log(strStr("sadbutsad", "sad"));
+console.log(strStr("leetcode", "leeto"));
